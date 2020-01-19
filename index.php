93c71a6e4ee63b1dc4f26c3b9d73878961f22a35
beach <beachwm2829@gmail.com>
2020-01-19 17:48:00 +0700

    nav bar

--- index.php
+++ index.php
@@ -7,11 +7,38 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+        <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/minty/bootstrap.min.css" rel="stylesheet" integrity="sha384-9NlqO4dP5KfioUGS568UFwM3lbWf3Uj3Qb7FBHuIuhLoDp3ZgAqPE1/MYLEBPZYM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+        <title>หน้าแรก | KAREE Cinema</title>
+        <style>
+            .mySlides {display:none;}
+        </style>
     </head>
     <body>
         <?php
-            echo 'halo111';
+            include './navigation/navbar.php';
         ?>
+        <div>
+            <image class="mySlides" src="img/11.jpg"  style="width: 100%; height: 90vh"> 
+            <image class="mySlides" src="img/12.jpg"  style="width: 100%; height: 90vh"> 
+            <image class="mySlides" src="img/13.jpg"  style="width: 100%; height: 90vh"> 
+        </div>   
+        <script>
+            var myIndex = 0;
+            carousel();
+            function carousel() {
+              var i;
+              var x = document.getElementsByClassName("mySlides");
+              for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";  
+              }
+              myIndex++;
+              if (myIndex > x.length) {myIndex = 1}    
+              x[myIndex-1].style.display = "block";  
+              setTimeout(carousel, 100); // Change image every 2 seconds
+            }
+        </script>
     </body>
 </html>
---
+++ navigation/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!--           <image src="imgaes/logo1.png" style="width: 120px; height: 65px; margin-right: 30px; margin-left: 20px;">-->
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+          <a class="nav-link" href="#" style="font-weight: bold; ">หน้าแรก <span class="sr-only">(current)</span></a>
+      </li> 
+      <li class="nav-item">
+          <a class="nav-link" href="#" style="font-weight: bold;">ภาพยนตร์</a>
+      </li>    
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="font-weight: bold;">โรงภาพยนตร์</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="font-weight: bold;">โปรโมชั่น</a>
+      </li>
+    </ul>
+    <a href="login.php">
+        <button class="btn my-2 my-sm-0 fa fa-user-circle" style="color: #fd9b30;font-size:30px;">
+<!--            <image src="imgaes/login.png" />-->
+            <h5 class="nav" style="color: #fd9b30">เข้าสู่ระบบ/สมัครสมาชิก</h5>
+        </button>
+    </a>
+  </div>
+</nav>
