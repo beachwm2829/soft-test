9c154e7e0cf239becc9c4fb9ef4508f403eb5a81
Akkarat <Akkarat@10.160.71.153>
2020-01-20 22:51:57 +0700

    uuu

--- DB.php
+++ DB.php
@@ -1,8 +1,46 @@
 <?php
-//ksdfjgberilukvgbekrjghkl4rjrbf4thg
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    class connectdb 
+{
+    public function connichdb(){
+        $dbhost = "localhost";
+        $dbuser = "root";
+        $dbpassword="";
+        $db="db_tester";
+        $conn = new mysqli($dbhost,$dbuser,$dbpassword,$db)or
+                die("Connect failed: %s\n".$conn->erro);
+        mysqli_set_charset($conn,"utf8");
+        return $conn;        
+    }
+    public function checkdb($user,$pass){
+         $sql = "SELECT * FROM `mem` WHERE mUser='".$user."' AND mPass='".$pass."'";
+         $result = mysqli_query($this->connichdb(), $sql);
+         if(mysqli_num_rows($result)==1)
+             {
+                  
+                $row= mysqli_fetch_array($result);
+                session_start();
+                $_SESSION['mUser']=$row['mName'];
+                
+                //header("location:memberpage.php");//หน้าสมาชิก
+            }
+         else{
+//             echo"Login Eror<br>";
+//             echo $sql;
+             header("location:login.php");
+         }
+    }
+    
+    public function regist($user,$pass,$name){
+         $sql = "INSERT INTO `member`(`name`, `user`, `pass`,) VALUES ('".$name."','".$user."','".$pass."')";
+         if(mysqli_query($this->connichdb(),$sql))
+         {
+             //echo 'Register OK';
+            header("location:login.php");
+         }
+        else{ echo "Connet F<br>";
+        echo $sql;
+        }
+     }
+     
+}
+     
