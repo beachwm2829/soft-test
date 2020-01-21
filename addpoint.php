0cb1a237543238ddd2073c87b94d74ac304e1288
Akkarat <Akkarat@10.160.71.153>
2020-01-21 17:28:20 +0700

    ทำUIของเพิ่มลบหนังให้ใหม่หน่อย

---
+++ addpoint.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="script.js" title="style1">
+        <script src="script.js">
+        
+        </script>
+        <title>ADD POINT</title>
+    </head>
+    <body background="logoV.png">
+    <center><div>
+        <div>
+            <form name="addp" action='sentpoint.php' method='POST' >
+                <input type="text" placeholder="USER ID" name='mid'/><br>
+                <input type='text' placeholder='POINT' name='point'/><br>
+                <button action='' onclick="a()">ADD</button>
+          </form>
+            
+        </div>
+      </div></center>
+    </body>
+</html>
